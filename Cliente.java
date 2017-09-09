82f6179d7fc8c985981feb20a5a1a73f85bffd09
atorresleticia <atorresleticia@gmail.com>
2017-09-09 17:06:53 -0300

    Calculo de tempo adicionado

--- Cliente.java
+++ Cliente.java
@@ -1,20 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-//package tcp;
-
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 
-/**
- *
- * @author herrmann
- */
 public class Cliente {
 
     public static final int PORTA = 8000;
@@ -23,17 +12,19 @@ public class Cliente {
     public static void main(String args[]) {
 
         int sizePacket = Integer.parseInt(args[1]);
-        System.out.println("sizePacket: " + sizePacket);
-        System.out.println("fileName: " + args[0]);
+		String fileName = args[0].substring(args[0].lastIndexOf("\\") + 1).trim();
+
+        System.out.println("Tamanho do Pacote: " + sizePacket);
+        System.out.println("Arquivo: " + fileName);
 
         try {
+           
             Socket socket = new Socket(SERVIDOR, PORTA);
             FileOutputStream file = new FileOutputStream(args[0]);
             BufferedOutputStream buffer = new BufferedOutputStream(file);
             InputStream in = socket.getInputStream();
 
             byte[] fileChunk = new byte[sizePacket];
-
             int bytesRead = 0;
 
             while ((bytesRead = in.read(fileChunk)) != -1) {
