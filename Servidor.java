12427c79b7375b6963a1d1d422e4daacebba31a1
Letícia Torres <atorresleticia@gmail.com>
2017-09-12 13:50:29 -0300

    Update Servidor.java

--- Servidor.java
+++ Servidor.java
@@ -1,15 +1,7 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 public class Servidor {
 
@@ -18,58 +10,54 @@ public class Servidor {
 
 	public static void main(String[] args) {
         
-        int sizePacket = Integer.parseInt(args[1]);
-        String fileName = args[0].substring(args[0].lastIndexOf("\\") + 1).trim();
-		//while(true){
-			try {
-				try (ServerSocket serverSocket = new ServerSocket(PORTA)) {
-					
-	                System.out.println(" Aguardando conexao...");
-					Socket socket = serverSocket.accept();
-					System.out.println("Conectado com: " + socket.getInetAddress().getHostAddress());
-	                System.out.println("Tamanho do pacote: " + sizePacket);
-	                System.out.println("Enviando arquivo: " + fileName);
-					
-	                File send = new File(args[0]);
-					long fileLength = send.length();
+    int sizePacket = Integer.parseInt(args[1]);
+    String fileName = args[0].substring(args[0].lastIndexOf("\\") + 1).trim();
+		
+		try {
+			try (ServerSocket serverSocket = new ServerSocket(PORTA)) {
 
-					FileInputStream file_send = new FileInputStream(send);
-					BufferedInputStream buff_file = new BufferedInputStream(file_send);
-					OutputStream output = socket.getOutputStream();
+				System.out.println(" Aguardando conexao...");
+				Socket socket = serverSocket.accept();
+				System.out.println("Conectado com: " + socket.getInetAddress().getHostAddress());
+				System.out.println("Tamanho do pacote: " + sizePacket);
+				System.out.println("Enviando arquivo: " + fileName);
 
-					byte[] fileChunk;
-					long postionPtr = 0; // "ponteiro"
-					
-					long startTime = System.nanoTime();
-					while (postionPtr != fileLength) {
-						if (fileLength - postionPtr >= sizePacket)
-							postionPtr += sizePacket;
-						else {
-							sizePacket = (int) (fileLength - postionPtr);
-							postionPtr = fileLength;
-						}
-						fileChunk = new byte[sizePacket];
-						buff_file.read(fileChunk, 0, sizePacket);
-						output.write(fileChunk);
+				File send = new File(args[0]);
+				long fileLength = send.length();
+				FileInputStream file_send = new FileInputStream(send);
+				BufferedInputStream buff_file = new BufferedInputStream(file_send);
+				OutputStream output = socket.getOutputStream();
+
+				byte[] fileChunk;
+				long postionPtr = 0;
+
+				long startTime = System.nanoTime();
+				while (postionPtr != fileLength) {
+					if (fileLength - postionPtr >= sizePacket)
+						postionPtr += sizePacket;
+					else {
+						sizePacket = (int) (fileLength - postionPtr);
+						postionPtr = fileLength;
 					}
-					long endTime = System.nanoTime();
-					
-					System.out.println("Arquivo enviado");
-					
-					BufferedWriter writer = new BufferedWriter(new FileWriter(new File("times_TCP.txt"), true));
-					writer.write(args[0] + "\t" + args[1] + "\t" + (endTime - startTime)/1000000 + "ms\n");
-									
-					output.flush();
-					socket.close();
-					serverSocket.close();
-					writer.close();
+					fileChunk = new byte[sizePacket];
+					buff_file.read(fileChunk, 0, sizePacket);
+					output.write(fileChunk);
 				}
+				long endTime = System.nanoTime();
 
-			} catch (IOException ex) {
-				Logger.getLogger("tcp").log(Level.SEVERE, null, ex);
+				System.out.println("Arquivo enviado");
+
+				BufferedWriter writer = new BufferedWriter(new FileWriter(new File("times_TCP.txt"), true));
+				writer.write(args[0] + "\t" + args[1] + "\t" + (endTime - startTime)/1000000 + "ms\n");
+
+				output.flush();
+				socket.close();
+				serverSocket.close();
+				writer.close();
 			}
-		//}
-        
-	}
 
+		} catch (IOException ex) {
+			Logger.getLogger("tcp").log(Level.SEVERE, null, ex);
+		}
+	}
 }
